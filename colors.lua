2ebe957aaa15ee2de0313e6a8942c9be2a17f223
JJSax <5084938+JJSax@users.noreply.github.com>
2022-09-10 17:20:07 -0600

    Initial Commit

---
+++ colors.lua
@@ -0,0 +1,166 @@
+local colors = {}
+colors.__index = colors
+colors._VERSION = "1.0.0"
+colors.range = 1
+
+-- local ffi = require("ffi")
+-- https://discord.com/channels/329400828920070144/329404715521802241/960992021965865070
+-- this is help for ffi.
+-- https://luajit.org/ext_ffi_tutorial.html
+
+local function clamp(x, min, max)
+	if x > max then return max end
+	if x < min then return min end
+	return x
+end
+
+local function testNumber(...)
+	for i = 1, select("#", ...) do
+		local v = select(i, ...)
+		if type(v) ~= "number" then
+			error("Expected number, got " .. type(v), 2)
+		end
+	end
+end
+
+function colors.new(r, g, b, a)
+	local self = setmetatable({}, colors)
+	if r and g and b then
+		testNumber(r, g, b)
+		self[1] = r
+		self[2] = g
+		self[3] = b
+		self[4] = a or colors.range
+	elseif type(r) == "table" then
+		testNumber(r[1], r[2], r[3])
+		self[1] = r[1]
+		self[2] = r[2]
+		self[3] = r[3]
+		self[4] = r[4] or colors.range
+	end
+	self.color = true
+	return self
+end
+
+
+function colors:lighten(amount)
+	assert(type(amount) == "number", "amount must be a number")
+	return colors.new(
+		clamp(self[1] + amount * self.range, 0, self.range),
+		clamp(self[2] + amount * self.range, 0, self.range),
+		clamp(self[3] + amount * self.range, 0, self.range),
+		self[4]
+	)
+end
+
+function colors:darken(amount)
+	assert(type(amount) == "number", "amount must be a number")
+	return colors.new(
+		clamp(self[1] - amount * self.range, 0, self.range),
+		clamp(self[2] - amount * self.range, 0, self.range),
+		clamp(self[3] - amount * self.range, 0, self.range),
+		self[4]
+	)
+end
+
+function colors:alpha(amount)
+	assert(type(amount) == "number", "amount must be a number")
+	return colors.new(
+		self[1],
+		self[2],
+		self[3],
+		clamp(amount * self.range, 0, self.range)
+	)
+end
+
+function colors.unpack(color)
+	return color[1], color[2], color[3], color[4]
+end
+
+function colors.__add(a, b)
+	return colors.new(a[1] + b[1], a[2] + b[2], a[3] + b[3], a[4] + b[4])
+end
+
+function colors.__sub(a, b)
+	return colors.new(a[1] - b[1], a[2] - b[2], a[3] - b[3], a[4] - b[4])
+end
+
+function colors.__mul(a, b)
+	if type(a) == "number" then
+		return colors.new(a * b[1], a * b[2], a * b[3], a * b[4])
+	elseif type(b) == "number" then
+		return colors.new(b * a[1], b * a[2], b * a[3], b * a[4])
+	else
+		return colors.new(a[1] * b[1], a[2] * b[2], a[3] * b[3], a[4] * b[4])
+	end
+end
+
+function colors.__div(a, b)
+	return colors.new(a[1] / b, a[2] / b, a[3] / b, a[4] / b)
+end
+
+function colors.__eq(a, b)
+	for i = 1, 4 do
+		if a[i] ~= b[i] then
+			return false
+		end
+	end
+	return a.color and b.color
+end
+
+function colors.average(a, b)
+	return colors.new((a[1] + b[1]) / 2, (a[2] + b[2]) / 2, (a[3] + b[3]) / 2, (a[4] + b[4]) / 2)
+end
+
+function colors.isValid(a)
+	if type(a) ~= "table" then return false end
+	for i = 1, 4 do
+		if type(a[i]) ~= "number" then
+			return false
+		end
+	end
+	return a.color == true -- ensure boolean return, not nil
+end
+
+-- colors in order from wikipedia web colors
+colors.white = colors.new(1, 1, 1)
+colors.silver = colors.new(0.75, 0.75, 0.75)
+colors.gray = colors.new(0.5, 0.5, 0.5)
+colors.black = colors.new(0, 0, 0)
+colors.red = colors.new(1, 0, 0)
+colors.maroon = colors.new(0.5, 0, 0)
+colors.yellow = colors.new(1, 1, 0)
+colors.olive = colors.new(0.5, 0.5, 0)
+colors.lime = colors.new(0, 1, 0)
+colors.green = colors.new(0, 0.5, 0)
+colors.aqua = colors.new(0, 1, 1)
+colors.teal = colors.new(0, 0.5, 0.5)
+colors.blue = colors.new(0, 0, 1)
+colors.navy = colors.new(0, 0, 0.5)
+colors.fuchsia = colors.new(1, 0, 1)
+colors.purple = colors.new(0.5, 0, 0.5)
+
+-- extras
+
+--
+colors.orange = colors.new(1, 0.5, 0)
+colors.cyan = colors.new(0, 1, 1)
+colors.magenta = colors.new(1, 0, 1)
+colors.lightGray = colors.new(0.827451, 0.827451, 0.827451)
+colors.darkGray = colors.new(0.25, 0.25, 0.25)
+colors.lightRed = colors.new(1, 0.5, 0.5)
+colors.pink = colors.lightRed
+colors.lightGreen = colors.new(0.5, 1, 0.5)
+colors.lightBlue = colors.new(0.5, 0.5, 1)
+colors.lightYellow = colors.new(1, 1, 0.5)
+colors.lightCyan = colors.new(0.5, 1, 1)
+colors.lightMagenta = colors.new(1, 0.5, 1)
+colors.darkRed = colors.new(0.5, 0, 0)
+colors.darkGreen = colors.new(0, 0.5, 0)
+colors.darkBlue = colors.new(0, 0, 0.5)
+colors.darkYellow = colors.new(0.5, 0.25, 0)
+colors.brown = colors.new(0.5, 0.25, 0)
+colors.darkSlateGray = colors.new(0.184314, 0.309804, 0.309804)
+colors.slateGray = colors.new(0.439216, 0.513725, 0.513725)
+
+return colors
