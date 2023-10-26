fce4bd6f926df6f2876a7013bb841eca33694732
JJSax <5084938+JJSax@users.noreply.github.com>
2023-10-26 01:08:54 -0600

    Notate functions with LuaCATS

--- colors.lua
+++ colors.lua
@@ -1,11 +1,19 @@
+---@meta
 
 local iMap = { r = 1, g = 2, b = 3, a = 4 }
+
+---@class Colors
+---@field r number
+---@field g number
+---@field b number
+---@field a number
+---@field color boolean
 local colors = {}
 colors.__index = function(t, key)
 	return iMap[key] and t[iMap[key]] or colors[key]
 end
 
-colors._VERSION = "1.0.3"
+colors._VERSION = "1.0.4"
 colors.range = 1
 
 local function clamp(x, min, max)
@@ -23,25 +31,32 @@ local function testNumber(...)
 	end
 end
 
+---@param r number|table<number,number,number,number?> Red value or {r,g,b[, a]}
+---@param g number? Green value
+---@param b number? Blue value
+---@param a number? Alpha value
+---@return Colors
 function colors.new(r, g, b, a)
 	local self = setmetatable({}, colors)
-	if r and g and b then
-		testNumber(r, g, b)
-		self[1] = r
-		self[2] = g
-		self[3] = b
-		self[4] = a or colors.range
-	elseif type(r) == "table" then
-		testNumber(r[1], r[2], r[3])
-		self[1] = r[1]
-		self[2] = r[2]
-		self[3] = r[3]
-		self[4] = r[4] or colors.range
-	end
+    if r and g and b then
+        testNumber(r, g, b)
+        self[1] = r
+        self[2] = g
+        self[3] = b
+        self[4] = a or colors.range
+    elseif type(r) == "table" then
+        testNumber(r[1], r[2], r[3])
+        self[1] = r[1]
+        self[2] = r[2]
+        self[3] = r[3]
+        self[4] = r[4] or colors.range
+    end
+	assert(self[1], "Improper color passed.")
 	self.color = true
 	return self
 end
 
+---@param intensity number float 0-1 of how much to desaturate
 function colors:desaturate(intensity)
 	local i = (self.r + self.g + self.b) / 3
 	local dr, dg, db = i - self.r, i - self.g, i - self.b
@@ -52,6 +67,7 @@ function colors:desaturate(intensity)
 	)
 end
 
+---@param amount number float 0-1 of how much to lighten
 function colors:lighten(amount)
 	assert(type(amount) == "number", "amount must be a number")
 	return colors.new(
@@ -62,6 +78,7 @@ function colors:lighten(amount)
 	)
 end
 
+---@param amount number float 0-1 of how much to darken
 function colors:darken(amount)
 	assert(type(amount) == "number", "amount must be a number")
 	return colors.new(
@@ -72,6 +89,7 @@ function colors:darken(amount)
 	)
 end
 
+---@param amount number float 0-1 of how much to alpha
 function colors:alpha(amount)
 	assert(type(amount) == "number", "amount must be a number")
 	return colors.new(
@@ -82,18 +100,29 @@ function colors:alpha(amount)
 	)
 end
 
+---@param color Colors Unpack table into individual elements.
+---@return number, number, number, number # The R, G, B, A values
 function colors.unpack(color)
 	return color[1], color[2], color[3], color[4]
 end
 
+---@param a Colors
+---@param b Colors
+---@return Colors
 function colors.__add(a, b)
 	return colors.new(a[1] + b[1], a[2] + b[2], a[3] + b[3], a[4] + b[4])
 end
 
+---@param a Colors
+---@param b Colors
+---@return Colors
 function colors.__sub(a, b)
 	return colors.new(a[1] - b[1], a[2] - b[2], a[3] - b[3], a[4] - b[4])
 end
 
+---@param a Colors
+---@param b Colors
+---@return Colors
 function colors.__mul(a, b)
 	if type(a) == "number" then
 		return colors.new(a * b[1], a * b[2], a * b[3], a * b[4])
@@ -104,27 +133,40 @@ function colors.__mul(a, b)
 	end
 end
 
+---@param a Colors
+---@param b Colors
+---@return Colors
 function colors.__div(a, b)
 	return colors.new(a[1] / b, a[2] / b, a[3] / b, a[4] / b)
 end
 
+---@param a Colors
+---@param b Colors
+---@return boolean
 function colors.__eq(a, b)
-	for i = 1, 4 do
-		if a[i] ~= b[i] then
-			return false
-		end
-	end
-	return a.color and b.color
+    for i = 1, 4 do
+        if a[i] ~= b[i] then
+            return false
+        end
+    end
+    return a.color and b.color
 end
 
+---@param a Colors
+---@param b Colors
+---@return Colors
 function colors.average(a, b)
 	return colors.new((a[1] + b[1]) / 2, (a[2] + b[2]) / 2, (a[3] + b[3]) / 2, (a[4] + b[4]) / 2)
 end
 
+---@param alpha number alpha to get color.
+---@return Colors
 function colors.random(alpha)
-	return colors.new(math.random(), math.random(), math.random(), alpha or 1)
+    return colors.new(math.random(), math.random(), math.random(), alpha or 1)
 end
 
+---@param a Colors
+---@return boolean
 function colors.isValid(a)
 	if type(a) ~= "table" then return false end
 	for i = 1, 4 do
