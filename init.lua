b13188b828fd1085b1bd3cc37c0b3c6e6ca02e66
JJSax <5084938+JJSax@users.noreply.github.com>
2024-04-10 16:58:18 -0600

    Allow modification via .rgba
    fixes #1

--- init.lua
+++ init.lua
@@ -10,8 +10,11 @@ local colors = {}
 colors.__index = function(t, key)
 	return iMap[key] and t[iMap[key]] or colors[key]
 end
+colors.__newindex = function(t, key, value)
+	rawset(t, iMap[key] or key, value)
+end
 
-colors._VERSION = "1.0.5"
+colors._VERSION = "1.0.6"
 colors.range = 1
 
 local function clamp(x, min, max)
@@ -36,19 +39,19 @@ end
 ---@return Colors
 function colors.new(r, g, b, a)
 	local self = setmetatable({}, colors)
-    if r and g and b then
-        testNumber(r, g, b)
-        self[1] = r
-        self[2] = g
-        self[3] = b
-        self[4] = a or colors.range
-    elseif type(r) == "table" then
-        testNumber(r[1], r[2], r[3])
-        self[1] = r[1]
-        self[2] = r[2]
-        self[3] = r[3]
-        self[4] = r[4] or colors.range
-    end
+	if r and g and b then
+		testNumber(r, g, b)
+		self[1] = r
+		self[2] = g
+		self[3] = b
+		self[4] = a or colors.range
+	elseif type(r) == "table" then
+		testNumber(r[1], r[2], r[3])
+		self[1] = r[1]
+		self[2] = r[2]
+		self[3] = r[3]
+		self[4] = r[4] or colors.range
+	end
 	assert(self[1], "Improper color passed.")
 	self.color = true
 	return self
@@ -143,12 +146,12 @@ end
 ---@param b Colors
 ---@return boolean
 function colors.__eq(a, b)
-    for i = 1, 4 do
-        if a[i] ~= b[i] then
-            return false
-        end
-    end
-    return a.color and b.color
+	for i = 1, 4 do
+		if a[i] ~= b[i] then
+			return false
+		end
+	end
+	return a.color and b.color
 end
 
 ---@param a Colors
@@ -158,10 +161,10 @@ function colors.average(a, b)
 	return colors.new((a[1] + b[1]) / 2, (a[2] + b[2]) / 2, (a[3] + b[3]) / 2, (a[4] + b[4]) / 2)
 end
 
----@param alpha number alpha to get color.
+---@param alpha? number alpha to get color.
 ---@return Colors
 function colors.random(alpha)
-    return colors.new(math.random(), math.random(), math.random(), alpha or 1)
+	return colors.new(math.random(), math.random(), math.random(), alpha or 1)
 end
 
 --- Clones a color
@@ -229,9 +232,9 @@ colors.slateGray = colors.new(0.439216, 0.513725, 0.513725)
 if love and love.graphics then
 
 	-- Only if using love and the graphics is enabled
-    function colors:set()
-        love.graphics.setColor(self)
-    end
+	function colors:set()
+		love.graphics.setColor(self)
+	end
 
 end
 
